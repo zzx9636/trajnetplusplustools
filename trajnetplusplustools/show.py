1a3e035774db928f78544a83e2c299f2978aee4b
Zixu Zhang <zixuz@princeton.edu>
2021-07-24 17:40:13 -0400

    use ipython display

--- trajnetplusplustools/show.py
+++ trajnetplusplustools/show.py
@@ -15,6 +15,7 @@ def canvas(image_file=None, **kwargs):
     if image_file:
         fig.savefig(image_file, dpi=300)
     display(fig)
+    print("try to show plots")
     fig.show()
     #plt.close(fig)
 
